fe28335973f2191cdbb5d16bd6a95414940a6699
Djody Kort <djody@pixelplus.nl>
2023-03-13 15:11:46 +0100

    big update, we now have a beginning of the admin dashboard

--- plugin2/files/php/includes/classes.php
+++ plugin2/files/php/includes/classes.php
@@ -2,25 +2,37 @@
 // ========== Imports =========
 include_once 'functions.php';
 
+// =========== Data Classes ===========
+class OGSettingsData {
+    public $settings = array(
+        /* Option group */ 'ppOGSettingsGroup' => array(/* Option name */'ggOGSettings' => array(
+            // Section 1 (Licensing)
+            /* SectionID */'ppOGSectionLicence' => array(/* Section Title */ 'Licentie' => array(
+                // Setting 1
+                /* settingFieldID */'ppOGSettingLicenceKey' => /* settingFieldTitle */ 'Licentie sleutel')
+            )
+        )
+    ));
+    }
 class OGAuthentication {
-	public $tableNames = array(
-		'object_data_bog_queue' => 'ppOG_dataBOG',
-		'object_data_bouwnummers_queue' => 'ppOG_dataBouwnummers',
-		'object_data_bouwtypen_queue' => 'ppOG_dataBouwTypen',
-		'object_data_nieuwbouw_queue' => 'ppOG_dataNieuwbouw',
-		'object_data_provincies' => 'ppOG_dataProvincies',
-		'object_data_wonen_queue' => 'ppOG_dataWonen',
-		'object_image_types' => 'ppOG_imageTypes',
-		'object_media_id' => 'ppOG_imageIDs',
-		'og_types' => 'ppOG_ogTypes',
-		'permalink_structure' => 'ppOG_permalinkStructure',
-		'object_media_queue' => 'ppOG_imageQueue'
-	);
+    public $tableNames = array(
+        'object_data_bog_queue' => 'ppOG_dataBOG',
+        'object_data_bouwnummers_queue' => 'ppOG_dataBouwnummers',
+        'object_data_bouwtypen_queue' => 'ppOG_dataBouwTypen',
+        'object_data_nieuwbouw_queue' => 'ppOG_dataNieuwbouw',
+        'object_data_provincies' => 'ppOG_dataProvincies',
+        'object_data_wonen_queue' => 'ppOG_dataWonen',
+        'object_image_types' => 'ppOG_imageTypes',
+        'object_media_id' => 'ppOG_imageIDs',
+        'og_types' => 'ppOG_ogTypes',
+        'permalink_structure' => 'ppOG_permalinkStructure',
+        'object_media_queue' => 'ppOG_imageQueue'
+    );
     public $sourceDBAuth = array(
-	    "hostname" => "s244.webhostingserver.nl",
-	    "username" => "deb142504_pixelplus",
-	    "password" => "100%procentVeiligWachtwoord",
-	    "database" => "deb142504_pixelplus"
+        "hostname" => "s244.webhostingserver.nl",
+        "username" => "deb142504_pixelplus",
+        "password" => "100%procentVeiligWachtwoord",
+        "database" => "deb142504_pixelplus"
     );
 }
 class WPColorScheme {
@@ -62,6 +74,30 @@ class WPColorScheme {
         return $_wp_admin_css_colors['fresh']->colors[2];
     }
 }
+
+// ==== Activation and Deactivation ====
+class OGActivationAndDeactivation {
+    // ======== Activation ========
+    function activate() {
+    }
+    // ======== Deactivation ========
+    function deactivate()
+    {
+        add_action('init', array($this, 'deletePostType'));
+    }
+
+    // ============ Functions ============
+    function deletePostType() {
+        // ==== Vars ====
+        $postType = 'post_type';
+        // ==== Start of Function ====
+        unregister_post_type($postType);
+    }
+
+}
+
+// ====== Excecuted every time the plugin is loaded ======
+// Settings Page
 class OGSettingsPage
 {
     function __construct()
@@ -131,16 +167,19 @@ class OGSettingsPage
             'pixelplus-og-plugin-aanbod-alv',
             array($this, 'htmlOGAanbodALV'));
     }
-
     // ==== Register Settings ====
     function registerSettings(): void {
+        // ==== Vars ====
+        $settings = new OGSettingsData();
+        $settingsPageSlug = 'pixelplus-og-plugin-settings';
+
+        // ==== Start of Function ====
         // Register Settings Section
         add_settings_section(
             'ppOG_section1',
             'Licensing',
             array($this, 'HTMLppOGSection1'),
             'pixelplus-og-plugin');
-
         // Register Settings Field
         add_settings_field(
             'ppOG_license_key',
@@ -151,13 +190,54 @@ class OGSettingsPage
 
         // Register Settings
         register_setting('ppOG', 'ppOG_license_key');
+
     }
 
     // ==== HTML ====
     // OG Admin
-    function HTMLOGAdminDashboard(): void { htmlHeader('OG Admin Dashboard'); ?>
-        <h1>test</h1>
-    <?php htmlFooter('OG Admin Dashboard');}
+    function HTMLOGAdminDashboard(): void {
+        // ======== Declaring Variables ========
+        // Classes
+        $customDB = new OGCustomDB();
+        $wpColorScheme = new WPColorScheme();
+        // Colors
+        $buttonColor = $wpColorScheme->returnColor();
+
+        // ======== Start of Function ========
+        if (isset($_POST['buttonSync'])) {
+            $customDB->syncTables();
+        }
+
+        htmlHeader('OG Admin Dashboard');?>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col' style='border-right: solid 1px black'>
+                    <h2 class='text-center'>Synchroniseer Aanbod</h2>
+                    <!-- ==== Button to sync the tables ==== -->
+                    <form method='post'>
+                    <div class='divAllAanbod clearfix'>
+                        <!-- Big Sync Button -->
+                        <button type='submit' name='buttonSync' style='background-color: <?php echo($buttonColor) ?>' class='text-center text-justify'>
+                            Volledige aanbod<br/>
+                            synchroniseren
+                        </button>
+                        <!-- Last sync time -->
+                        <div class='mt-3 d-table'>
+                            <img class='float-left me-2' src='<?php echo(plugins_url('img/recent-logo.png', dirname(__DIR__))) ?>' width='37px'>
+                            <span class='text-center align-middle'>Laatste synchronisatie: vandaag 14.15</span>
+                        </div>
+                    </div>
+
+                    <!-- 4 Smaller Sync Buttons -->
+                    </form>
+                </div>
+
+                <div class='col'>
+                    <h2 class='text-center'>Statistieken</h2>
+                </div>
+            </div>
+        </div>
+        <?php htmlFooter('OG Admin Dashboard');}
     function HTMLOGAdminSettings(): void { htmlHeader('OG Admin Settings'); ?>
         <form method="post" action="options.php">
             <?php settings_fields('ppOG'); ?>
@@ -175,8 +255,8 @@ class OGSettingsPage
         <?php }
     // OG Aanbod
     function HTMLOGAanbodDashboard(): void { htmlHeader('OG Aanbod Dashboard'); ?>
-            <h1>test</h1>
-        <?php htmlFooter('OG Aanbod Dashboard');}
+
+    <?php htmlFooter('OG Aanbod Dashboard');}
     function HTMLOGAanbodWonen(): void { htmlHeader('OG Wonen'); ?>
         <h1>test</h1>
     <?php htmlFooter('OG Aanbod Wonen');}
@@ -190,6 +270,7 @@ class OGSettingsPage
         <h1>test</h1>
     <?php htmlFooter('OG Aanbod A&LV');}
 }
+
 
 class OGCustomDB {
     // ==== Declaring Variables ====
@@ -250,7 +331,6 @@ class OGCustomDB {
     }
 	function syncTables(): void {
 		// ======== Declaring Variables ========
-
 
 		// ======== Start of Function ========
         echo('Syncing Tables'.PHP_EOL);
--- plugin2/pp-og-koppeling.php
+++ plugin2/pp-og-koppeling.php
@@ -7,12 +7,17 @@
  * Author: Pixelplus - Djody
  * Author URI: https://djody.nl/
  **/
+
 // ========= Imports =========
 include_once 'files/php/includes/classes.php';
 include_once 'files/php/includes/functions.php';
 
 // ============ Declaring Variables ============
+$activateAndDeactivate = new OGActivationAndDeactivation();
+
 $settingsPage = new OGSettingsPage();
-$customDB = new OGCustomDB();
 
 // ============ Start of Program ============
+// Activation and Deactivation and Uninstall
+register_activation_hook(__FILE__, array($activateAndDeactivate, 'activate'));
+register_deactivation_hook(__FILE__, array($activateAndDeactivate, 'deactivate'));
